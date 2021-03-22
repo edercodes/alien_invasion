fc692331e3cc873888afe23412b10156d8bc3899
calderoneder <64505582+calderoneder@users.noreply.github.com>
2021-03-22 10:24:09 -0700

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,10 @@
 import sys ### this is my own code
 
-import pygame
+import pygame ### imports pygame from terminal installation
 
-from settings import Settings
-from ship import Ship
+from settings import Settings ### imports Settings module for adjustments to game
+from ship import Ship ### imports Ship module with additional settings
+from bullet import Bullet ### imports Bullet module with adjustments
 
 class AlienInvasion:
     """Overall class to manage game assets and bahavior."""
@@ -19,6 +20,7 @@ class AlienInvasion:
         pygame.display.set_caption("AlienInvasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group() ### groups is used to draw bullets to screen and store used bullets
 
 
     def run_game(self):
@@ -26,7 +28,17 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update() ### ship updates in response to player input and processed before screen is updated
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update() ### updates for each bullet placed in the group
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy(): ### copy method allows for bullet modifications inside the loop
+            if bullet.rect.bottom <= 0: ### checks to see if bullet has disappeared from the screen
+                self.bullets.remove(bullet) ### removed from bullets if it disappears
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -46,6 +58,14 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet() ### calls to fire bullets on screen when spacebnar is pressed
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -58,6 +78,8 @@ class AlienInvasion:
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
