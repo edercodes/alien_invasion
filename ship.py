765ec70336ec071f4d8f9c68ca8f161570d54c8b
calderoneder <64505582+calderoneder@users.noreply.github.com>
2021-03-30 13:52:15 -0700

    Update ship.py

--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship:
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        # Load the ship imafe and get its rect.
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
@@ -37,3 +37,8 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x) ### allows to track ships exact position
