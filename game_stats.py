cdee78ce161500ef9fd56c300f0dbf50b4e9bd74
calderoneder <64505582+calderoneder@users.noreply.github.com>
2021-03-30 19:14:19 -0700

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -5,7 +5,10 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        
+
+        # Start the game in an inactive state.
+        self.game_active = False 
+
         # Start Alien Invasion in an active state.
         self.game_active = True
 
