8ce3b6094b26d039b5fc1e183fa32d2552ae2228
calderoneder <64505582+calderoneder@users.noreply.github.com>
2021-03-31 16:29:50 -0700

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -38,17 +38,17 @@ class Scoreboard:
 
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
-        high_score = round(self.stats.high_score, -1)   ### rounds high score to nearest 10 and formats with commas
+        high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True,    ### generates image from the score
+        self.high_score_image = self.font.render(high_score_str, True,
                 self.text_color, self.settings.bg_color)
 
         # Center the high score at the top of the screen.
         self.high_score_rect = self.high_score_image.get_rect()
-        self.high_score_rect.centerx = self.screen_rect.centerx    ### centers the high score horizonatally
-        self.high_score_rect.top = self.score_rect.top    ### sets 'top' attribute to match the top of the score image
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
 
-    def prep_level(self):   ### method creates image from values stored in stats.level
+    def prep_level(self):
         """Turn the level into a rendered image."""
         level_str = str(self.stats.level)
         self.level_image = self.font.render(level_str, True,
@@ -56,8 +56,8 @@ class Scoreboard:
 
         # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
-        self.level_rect.right = self.score_rect.right   ### sets both image attributes to the right
-        self.level_rect.top = self.score_rect.bottom + 10   ### respositions 'top' attribute
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_ships(self):
         """Show how many ships are left."""
